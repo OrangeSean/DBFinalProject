1ae569eb6f83759ace7dda90190d71443fb9c4e0
Celine Patag <chibeepatag@users.noreply.github.com>
2014-04-14 21:10:55 +0930

    instructions

--- initialize.sql
+++ initialize.sql
@@ -1,3 +1,15 @@
+--Sean's local (Change to point to your local, comment out mine below and run in sqlplus
+-- please don't delete mine below so we dont have to keep changing. Thanks!
+spool C:\Users\ChibeePatag\git\DBFinalProject\session.txt
+start C:\Users\ChibeePatag\git\DBFinalProject\Homer.sql;
+start C:\Users\ChibeePatag\git\DBFinalProject\skill.sql;
+start C:\Users\ChibeePatag\git\DBFinalProject\department.sql;
+start C:\Users\ChibeePatag\git\DBFinalProject\employee.sql;
+start C:\Users\ChibeePatag\git\DBFinalProject\department2.sql;
+start C:\Users\ChibeePatag\git\DBFinalProject\training.sql;
+spool out;
+
+--Celine's Local
 spool C:\Users\ChibeePatag\git\DBFinalProject\session.txt
 start C:\Users\ChibeePatag\git\DBFinalProject\Homer.sql;
 start C:\Users\ChibeePatag\git\DBFinalProject\skill.sql;
