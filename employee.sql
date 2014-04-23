a525fe6e769c3f0c40a257ffa5e7a2337480977e
OrangeSean <haoxin418@hotmail.com>
2014-04-23 12:30:47 +0930

    PartB-5 complete. Modified the employee whos number is 16. Update his hiring date from 2011 to 2013.

--- employee.sql
+++ employee.sql
@@ -14,7 +14,7 @@ Insert into EMPLOYEE (EMP_NUM,EMP_LAST,EMP_FIRST,HIRE_DATE,DOB,SUPER_ID,DEPT_COD
 Insert into EMPLOYEE (EMP_NUM,EMP_LAST,EMP_FIRST,HIRE_DATE,DOB,SUPER_ID,DEPT_CODE) values (9,'Baldwin','Sybil',to_date('14-FEB-09','DD-MON-RR'),to_date('19-JUN-73','DD-MON-RR'),2,'ACCTG');
 Insert into EMPLOYEE (EMP_NUM,EMP_LAST,EMP_FIRST,HIRE_DATE,DOB,SUPER_ID,DEPT_CODE) values (10,'Brock','Ava',to_date('06-FEB-11','DD-MON-RR'),to_date('26-JUN-63','DD-MON-RR'),2,'ACCTG');
 Insert into EMPLOYEE (EMP_NUM,EMP_LAST,EMP_FIRST,HIRE_DATE,DOB,SUPER_ID,DEPT_CODE) values (11,'Matthews','Ori',to_date('29-JAN-10','DD-MON-RR'),to_date('11-JUN-72','DD-MON-RR'),2,'ACCTG');
-Insert into EMPLOYEE (EMP_NUM,EMP_LAST,EMP_FIRST,HIRE_DATE,DOB,SUPER_ID,DEPT_CODE) values (12,'Holloway','Wynter',to_date('16-JAN-11','DD-MON-RR'),to_date('12-FEB-69','DD-MON-RR'),3,'OPRTN');
+Insert into EMPLOYEE (EMP_NUM,EMP_LAST,EMP_FIRST,HIRE_DATE,DOB,SUPER_ID,DEPT_CODE) values (12,'Holloway','Wynter',to_date('16-JAN-13','DD-MON-RR'),to_date('12-FEB-69','DD-MON-RR'),3,'OPRTN');
 Insert into EMPLOYEE (EMP_NUM,EMP_LAST,EMP_FIRST,HIRE_DATE,DOB,SUPER_ID,DEPT_CODE) values (13,'Swanson','Margaret',to_date('07-JAN-11','DD-MON-RR'),to_date('27-SEP-79','DD-MON-RR'),3,'OPRTN');
 Insert into EMPLOYEE (EMP_NUM,EMP_LAST,EMP_FIRST,HIRE_DATE,DOB,SUPER_ID,DEPT_CODE) values (14,'Acevedo','Jolie',to_date('05-JAN-08','DD-MON-RR'),to_date('03-AUG-72','DD-MON-RR'),3,'OPRTN');
 Insert into EMPLOYEE (EMP_NUM,EMP_LAST,EMP_FIRST,HIRE_DATE,DOB,SUPER_ID,DEPT_CODE) values (15,'Faulkner','Noah',to_date('30-NOV-11','DD-MON-RR'),to_date('21-AUG-65','DD-MON-RR'),3,'OPRTN');
