084d1cf6f1c2f27b0bf34ee34a019aa619a51bc2
Celine Patag <chibeepatag@users.noreply.github.com>
2014-04-25 21:53:43 +0930

    Fixed date on subtotal

--- partB/partB-8.sql
+++ partB/partB-8.sql
@@ -14,31 +14,30 @@ column PYTHO format 9
 column MOBDA format 9
 column "Latest Date Acquired" heading "Latest|Date|Acquired" format A12
 
-select decode(emp_num, null, ' ', emp_num) id, 
-decode(emp_first || ' ' || emp_last, ' ', 'Number of Trainings', emp_first || ' ' || emp_last) "Employee Name",
-sum(decode(code, 'JAVA1', 1, 0)) "JAVA1",
-nvl(to_char(max(case when code = 'JAVA1' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ') as "Latest Date Acquired",
-sum(decode(code, 'JAVA2', 1, 0)) "JAVA2",
-nvl(to_char(max(case when code = 'JAVA2' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ') as "Latest Date Acquired",
-sum(decode(code, 'J2EE', 1, 0)) "J2EE",
-nvl(to_char(max(case when code = 'J2EE' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ') as "Latest Date Acquired",
-sum(decode(code, 'MOBDA', 1, 0)) "MOBDA",
-nvl(to_char(max(case when code = 'MOBDA' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ') as "Latest Date Acquired",
-sum(decode(code, 'PRJM1', 1, 0)) "PRJM1",
-nvl(to_char(max(case when code = 'PRJM1' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ') as "Latest Date Acquired",
-sum(decode(code, 'PRJM2', 1, 0)) "PRJM2",
-nvl(to_char(max(case when code = 'PRJM2' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ') as "Latest Date Acquired", 
-sum(decode(code, 'PYTHO', 1, 0)) "PYTHO",
-nvl(to_char(max(case when code = 'PYTHO' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ') as "Latest Date Acquired",
-sum(decode(code, 'RUBY', 1, 0)) "RUBY",
-nvl(to_char(max(case when code = 'RUBY' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ') as "Latest Date Acquired",
-sum(decode(code, 'SQL1', 1, 0)) "SQL1",
-nvl(to_char(max(case when code = 'SQL1' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ') as "Latest Date Acquired",
-sum(decode(code, 'SQL2', 1, 0)) "SQL2",
-nvl(to_char(max(case when code = 'SQL2' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ') as "Latest Date Acquired",
-count(code)
-from employee join training using (emp_num)
-group by GROUPING SETS 
-  ((emp_num, emp_first, emp_last), ());
+ select decode(emp_num, null, ' ', emp_num) id,
+ decode(emp_first || ' ' || emp_last, ' ', 'Number of Trainings', emp_first || ' ' || emp_last) "Employee Name",
+ sum(decode(code, 'JAVA1', 1, 0)) "JAVA1",
+ decode(emp_num, null, ' ', nvl(to_char(max(case when code = 'JAVA1' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ')) as "Latest Date Acquired",
+ sum(decode(code, 'JAVA2', 1, 0)) "JAVA2",
+ decode(emp_num, null, ' ', nvl(to_char(max(case when code = 'JAVA2' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ')) as "Latest Date Acquired",
+ sum(decode(code, 'J2EE', 1, 0)) "J2EE",
+ decode(emp_num, null, ' ', nvl(to_char(max(case when code = 'J2EE' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ')) as "Latest Date Acquired",
+ sum(decode(code, 'MOBDA', 1, 0)) "MOBDA",
+ decode(emp_num, null, ' ', nvl(to_char(max(case when code = 'MOBDA' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ')) as "Latest Date Acquired",
+ sum(decode(code, 'PRJM1', 1, 0)) "PRJM1",
+ decode(emp_num, null, ' ', nvl(to_char(max(case when code = 'PRJM1' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ')) as "Latest Date Acquired",
+ sum(decode(code, 'PRJM2', 1, 0)) "PRJM2",
+ decode(emp_num, null, ' ', nvl(to_char(max(case when code = 'PRJM2' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ')) as "Latest Date Acquired",
+ sum(decode(code, 'PYTHO', 1, 0)) "PYTHO",
+ decode(emp_num, null, ' ', nvl(to_char(max(case when code = 'PYTHO' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ')) as "Latest Date Acquired",
+ sum(decode(code, 'RUBY', 1, 0)) "RUBY",
+ decode(emp_num, null, ' ', nvl(to_char(max(case when code = 'RUBY' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ')) as "Latest Date Acquired",
+ sum(decode(code, 'SQL1', 1, 0)) "SQL1",
+ decode(emp_num, null, ' ', nvl(to_char(max(case when code = 'SQL1' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ')) as "Latest Date Acquired",
+ sum(decode(code, 'SQL2', 1, 0)) "SQL2",
+ decode(emp_num, null, ' ', nvl(to_char(max(case when code = 'SQL2' then  date_acquired else null end), 'dd-Mon-yyyy'), ' ')) as "Latest Date Acquired",
+ count(code)
+ from employee join training using (emp_num)
+ group by GROUPING SETS ((emp_num, emp_first, emp_last), ());
   
 set linesize 180
